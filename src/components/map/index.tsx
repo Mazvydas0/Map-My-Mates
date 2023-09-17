0bbcb1c2f955824f99c95dbe365430e6dd0f9898
Mazvydas0 <mazvydas.svirskas@gmail.com>
2023-09-17 18:15:02 +0200

    Added map in front page

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,3 +1,18 @@
-export default function index() {
-  return <div></div>;
+"use client";
+import Map from "react-map-gl";
+
+export default function MateMap() {
+  return (
+    <div className="flex-1 h-full">
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 12,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </div>
+  );
 }
