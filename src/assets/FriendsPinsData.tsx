be8a2c243d482691cdc1700c425a54e4271dbc9a
Mazvydas0 <mazvydas.svirskas@gmail.com>
2023-09-17 18:22:52 +0200

    Added markers, popups on the map

---
+++ src/assets/FriendsPinsData.tsx
@@ -0,0 +1,21 @@
+import profile from "/public/profile.png";
+
+let friendsPins = [
+  {
+    Name: "John",
+    Location: { Latitude: 37.8, Longitude: -122.4 },
+    profile: profile,
+  },
+  {
+    Name: "Mike",
+    Location: { Latitude: 37.6879, Longitude: -122.4702 },
+    profile: profile,
+  },
+  {
+    Name: "Luke",
+    Location: { Latitude: 37.8044, Longitude: -122.2712 },
+    profile: profile,
+  },
+];
+
+export default friendsPins;
